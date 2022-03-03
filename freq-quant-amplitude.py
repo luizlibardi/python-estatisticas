167cb4f806a3e36c80ff1e0abf6eae4a9f3d5f6e
Luiz Fernando Libardi de Sá <luizfernando@MMB027830CPS.local>
2022-03-03 15:00:46 -0300

    formula de amplitude fixa

---
+++ freq-quant-amplitude.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+import pandas as pd
+
+dados = pd.read_csv('./files/dados.csv')
+
+n = dados.shape[0]
+k = 1 + (10/3) * np.log10(n)
+print(int(k.round(0)))
+
+
+frequencia = pd.value_counts(
+    pd.cut(
+        x = dados.Renda,
+        bins = 17,
+        include_lowest=True
+    ),
+    sort = False
+)
+
+percentual = pd.value_counts(
+    pd.cut(
+        x = dados.Renda,
+        bins = 17,
+        include_lowest=True
+    ),
+    sort = False,
+    normalize = True
+)
+
+amplitude_fixa = pd.DataFrame({'Frequência': frequencia, 'Porcentagem (%)': percentual})
+amplitude_fixa.sort_index(ascending= False, inplace= True)
+print(amplitude_fixa)
