11c187d79e4f792c84e0dc2cacff7e053651b0d8
Luiz Fernando Libardi de Sá <CIANDT\luizfernando@lnb035338cps.ciandt.global>
2022-03-15 14:10:13 -0300

    medidas de dispersao - Desvio medio absoluto, Variancia e Desvio Padrao

--- medidas_de_dispersão.py
+++ medidas_de_dispersão.py
@@ -1,6 +1,9 @@
+## Medidas de Dispersão
+
 ## Desvio Médio Absoluto
 
 import pandas as pd
+import numpy as np
 
 dados = pd.read_csv('files/dados.csv')
 
@@ -30,4 +33,40 @@ notas_fulano['|Desvio|'] = notas_fulano['Desvio'].abs()
 notas_fulano['|Desvio|'].mean()
 
 desvio_medio_absoluto = notas_fulano['Fulano'].mad() # No pandas
-print(desvio_medio_absoluto)
+# print(desvio_medio_absoluto)
+
+## Variância
+# Variancia Populacional
+# Variancia Amostral
+
+notas_fulano['(Desvio^2'] = notas_fulano['Desvio'].pow(2)
+# print(notas_fulano)
+
+notas_fulano['(Desvio^2'].sum() / (len(notas_fulano) - 1)
+
+variancia = notas_fulano['Fulano'].var() # No pandas
+# print(variancia)
+
+## Desvio Padrão
+
+# print(np.sqrt(variancia))
+
+desvio_padrao = notas_fulano['Fulano'].std()
+# print(desvio_padrao)
+
+df.mean()
+df.median()
+df.mode()
+df.std()
+
+
+# dataset = pd.DataFrame({
+#     'Sexo': ['H', 'M', 'M', 'M', 'M', 'H', 'H', 'H', 'M', 'M'],
+#     'Idade': [53, 72, 54, 27, 30, 40, 58, 32, 44, 51]
+# })
+
+# selecao = (dataset['Sexo'] == 'M')
+
+# mulheres = dataset[selecao]
+
+# print(mulheres['Idade'].std())
